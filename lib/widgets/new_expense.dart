d588c164020404489385453a7055cecfddf816c5
Harry Huang <psyhh4@exmail.nottingham.ac.uk>
2024-06-11 13:19:49 +0100

    back to normal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -81,91 +81,12 @@ class _NewExpenseState extends State<NewExpense> {
               padding: EdgeInsets.fromLTRB(16, 16, 16, keyBoardSpace + 16),
               child: Column(
                 children: [
-                  if (width >= 600)
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            controller: _titleController,
-                            decoration: const InputDecoration(
-                              label: Text('Title'),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 24),
-                        Expanded(
-                          child: TextField(
-                            controller: _amountController,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              prefixText: '£ ',
-                              label: Text('Amount'),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  else
                     TextField(
                       controller: _titleController,
                       decoration: const InputDecoration(
                         label: Text('Title'),
                       ),
                     ),
-                  if (width >= 600)
-                    Row(children: [
-                      DropdownButton(
-                          value: _selectedCategory,
-                          items: Category.values
-                              .map(
-                                (category) => DropdownMenuItem(
-                                  value: category,
-                                  child: Text(
-                                    category.name.toUpperCase(),
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                          onChanged: (value) {
-                            if (value == null) {
-                              return;
-                            }
-                            setState(() {
-                              _selectedCategory = value;
-                            });
-                          }),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(_selectedDate == null
-                                ? 'No date selected'
-                                : formatter.format(_selectedDate!)),
-                            IconButton(
-                                onPressed: _dateTimePicker,
-                                icon: const Icon(Icons.calendar_month)),
-                          ],
-                        ),
-                      )
-                    ]),
-                  if (width >= 600)
-                    Row(children: [
-                      const Spacer(),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cancel'),
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            _submitExpenseData();
-                          },
-                          child: const Text('Save')),
-                    ])
-                  else
                     Row(
                       children: [
                         Expanded(
